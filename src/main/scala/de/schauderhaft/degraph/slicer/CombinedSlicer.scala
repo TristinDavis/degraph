d4489af76a7f5f8228778d00346e4f84c124128e
Jens Schauder <jens.schauder@freenet.de>
2013-03-12 07:13:07 +0100

    removed more unneeded classes

--- src/main/scala/de/schauderhaft/degraph/slicer/CombinedSlicer.scala
+++
@@ -1,7 +0,0 @@
-package de.schauderhaft.degraph.slicer
-
-import de.schauderhaft.degraph.model.Node
-
-class CombinedSlicer(slicer: (AnyRef => Node)*) extends (AnyRef => Node) {
-    def apply(n: AnyRef): Node = slicer.foldLeft(n.asInstanceOf[Node])((x, s) => if (x != n) x else s(n))
-}
--- src/test/scala/de/schauderhaft/degraph/graph/NodeTestUtil.scala
+++
@@ -1,7 +0,0 @@
-package de.schauderhaft.degraph.graph
-
-import de.schauderhaft.degraph.model.SimpleNode
-
-object NodeTestUtil {
-    def n(s: String) = SimpleNode(s, s)
-}
