f1b80e408f8908c036d1215397bf630b96fb2918
Jens Schauder <jens.schauder@freenet.de>
2014-11-27 07:50:16 +0100

    Baseclass for analyzers extracted

--- core/src/main/scala/de/schauderhaft/degraph/analysis/asm/Analyzer.scala
+++ core/src/main/scala/de/schauderhaft/degraph/analysis/asm/Analyzer.scala
@@ -1,43 +1,16 @@
 package de.schauderhaft.degraph.analysis.asm
 
-import de.schauderhaft.degraph.analysis.{NoSelfReference, AnalyzerLike}
 import de.schauderhaft.degraph.model.Node
 import de.schauderhaft.degraph.graph.Graph
-import org.objectweb.asm._
-import java.io.{InputStream, File, BufferedInputStream, FileInputStream}
-import java.util.zip.ZipFile
 
-object Analyzer extends AnalyzerLike {
+object Analyzer extends StreamAnalizer {
 
-  def analyze(sourceFolder: String, categorizer: (Node) => Node, filter: (Node) => Boolean): Graph = {
-
-    val parser = new StreamToGraphParser(categorizer, filter)
-
-    def analyze(f: File) = {
-      if (f.getName.endsWith(".class")) {
-        parser.parseStream(new FileInputStream(f))
-      } else {
-        val zipFile = new ZipFile(f)
-        val entries = zipFile.entries()
-        while (entries.hasMoreElements) {
-          val e = entries.nextElement()
-          if (e.getName.endsWith(".class")) {
-            parser.parseStream(zipFile.getInputStream(e))
-          }
-        }
-      }
-    }
-
-    for (folder <- sourceFolder.split(System.getProperty("path.separator"))) {
-      val fileFinder = new FileFinder(folder)
-      val files = fileFinder.find()
-      for {f <- files} {
-        analyze(f)
-      }
-    }
-    parser.g
+  def analyze(
+               sourceFolder: String,
+               categorizer: (Node) => Node,
+               filter: (Node) => Boolean): Graph = {
+    parse(sourceFolder, new StreamToGraphParser(categorizer, filter))
   }
-
 
 }
 
---
+++ core/src/main/scala/de/schauderhaft/degraph/analysis/asm/StreamAnalizer.scala
@@ -0,0 +1,38 @@
+package de.schauderhaft.degraph.analysis.asm
+
+import java.io.{FileInputStream, File}
+import java.util.zip.ZipFile
+
+import de.schauderhaft.degraph.analysis.AnalyzerLike
+import de.schauderhaft.degraph.graph.Graph
+
+abstract class StreamAnalizer extends AnalyzerLike {
+
+  def parse(sourceFolder: String, parser: StreamToGraphParser): Graph = {
+    def analyze(f: File) = {
+      if (f.getName.endsWith(".class")) {
+        parser.parseStream(new FileInputStream(f))
+      } else {
+        val zipFile = new ZipFile(f)
+        val entries = zipFile.entries()
+        while (entries.hasMoreElements) {
+          val e = entries.nextElement()
+          if (e.getName.endsWith(".class")) {
+            parser.parseStream(zipFile.getInputStream(e))
+          }
+        }
+      }
+    }
+
+    for (folder <- sourceFolder.split(System.getProperty("path.separator"))) {
+      val fileFinder = new FileFinder(folder)
+      val files = fileFinder.find()
+      for {
+        f <- files
+      } {
+        analyze(f)
+      }
+    }
+    parser.g
+  }
+}
