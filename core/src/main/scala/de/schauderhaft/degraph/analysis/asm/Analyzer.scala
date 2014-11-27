c31a28a543cef8905a0f90a609c5579e3f7e4f8d
Jens Schauder <jens.schauder@freenet.de>
2014-11-27 07:29:42 +0100

    separated the taking appart of the jar files  from actually parsing the streams

--- core/src/main/scala/de/schauderhaft/degraph/analysis/asm/Analyzer.scala
+++ core/src/main/scala/de/schauderhaft/degraph/analysis/asm/Analyzer.scala
@@ -4,26 +4,25 @@ import de.schauderhaft.degraph.analysis.{NoSelfReference, AnalyzerLike}
 import de.schauderhaft.degraph.model.Node
 import de.schauderhaft.degraph.graph.Graph
 import org.objectweb.asm._
-import java.io.{File, BufferedInputStream, FileInputStream}
+import java.io.{InputStream, File, BufferedInputStream, FileInputStream}
 import java.util.zip.ZipFile
 
 object Analyzer extends AnalyzerLike {
 
   def analyze(sourceFolder: String, categorizer: (Node) => Node, filter: (Node) => Boolean): Graph = {
-    val g = new Graph(categorizer, filter, new NoSelfReference(categorizer))
 
-    def analyze(f : File)={
+    val parser = new StreamToGraphParser(categorizer, filter)
+
+    def analyze(f: File) = {
       if (f.getName.endsWith(".class")) {
-        val reader = new ClassReader(new BufferedInputStream(new FileInputStream(f)))
-        reader.accept(new GraphBuildingClassVisitor(g), 0)
+        parser.parseStream(new FileInputStream(f))
       } else {
         val zipFile = new ZipFile(f)
         val entries = zipFile.entries()
         while (entries.hasMoreElements) {
           val e = entries.nextElement()
           if (e.getName.endsWith(".class")) {
-            val reader = new ClassReader(zipFile.getInputStream(e))
-            reader.accept(new GraphBuildingClassVisitor(g), 0)
+            parser.parseStream(zipFile.getInputStream(e))
           }
         }
       }
@@ -36,10 +35,18 @@ object Analyzer extends AnalyzerLike {
         analyze(f)
       }
     }
-
-    g
+    parser.g
   }
 
 
 }
 
+private class StreamToGraphParser(categorizer: (Node) => Node, filter: (Node) => Boolean) {
+
+  val g = new Graph(categorizer, filter, new NoSelfReference(categorizer))
+
+  def parseStream(is: InputStream): Unit = {
+    val reader = new ClassReader(new BufferedInputStream(is))
+    reader.accept(new GraphBuildingClassVisitor(g), 0)
+  }
+}
